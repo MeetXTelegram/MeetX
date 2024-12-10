76c3d631631fdd9ddc1a3f389f1d89468638d75e
username000101 <cxx.username000101@gmail.com>
2024-12-10 19:57:36 +0300

    Some changes; skip this commit

--- include/Init.hpp
+++ include/Init.hpp
@@ -9,7 +9,20 @@
 #include <PSQLWrapper.hpp>
 
 namespace init {
+    inline void initLoggers() {
+        auto stdoutSink = createStdoutSink;
+        auto fileSink = createFileSink("/home/username000101/SPDLOG_MEETX_TEST.txt");
+        loggingUtils::createLogger("SignalHandler", stdoutSink, fileSink, spdlog::level::trace);
+        loggingUtils::createLogger("MAIN_LOGGER", stdoutSink, fileSink, spdlog::level::trace);
+        loggingUtils::createLogger("ArgsHandler", stdoutSink, fileSink, spdlog::level::trace);
+        loggingUtils::createLogger("TerminateHandler", stdoutSink, fileSink, spdlog::level::trace);
+        loggingUtils::createLogger("PSQL", stdoutSink, fileSink, spdlog::level::trace);
+    }
+
     inline void MeetXInit(ah::Args& argsHandler) {
+        std::signal(SIGUSR1, signalsUtils::SIGUSR1Handler);
+        std::set_terminate(exceptionsUtils::TerminateHandler);
+
         if (!argsHandler.getArg("dbname").has_value()) {
             spdlog::log(spdlog::level::critical, "Failed to init MeetX: the \"dbname\" argument is not provided");
             std::raise(SIGUSR1);
@@ -21,10 +34,5 @@ namespace init {
             std::raise(SIGUSR1);
         }
         auto password = argsHandler.getArg("password").value().second;
-
-        auto stdoutSink = createStdoutSink;
-        auto fileSink = createFileSink("/home/username000101/SPDLOG_MEETX_TEST.txt");
-        loggingUtils::createLogger("MAIN_LOGGER", stdoutSink, fileSink, spdlog::level::trace);
-        loggingUtils::createLogger("ArgsHandler", stdoutSink, fileSink, spdlog::level::trace);
     }
 }
--- main.cpp
+++ main.cpp
@@ -4,17 +4,12 @@
 
 #include <Args.hpp>
 #include <Definitions.hpp>
+#include <Init.hpp>
 #include <PSQLWrapper.hpp>
 
-#include <Init.hpp>
-
-using namespace std;
-
 int main(int argc, char* argv[]) {
+    init::initLoggers();
     ah::Args args(argc, argv);
     args.process(ah::Args::ArgsStyle::LIKE_CMAKE);
     init::MeetXInit(args);
-    std::vector<std::string> files = {"/path/to/file1.ext", "/path/to/file2.ext"};
-    spdlog::log(spdlog::level::info, parsers::castMediaVector(files));
-    return 0;
 }
