6d525562f1a12540a02d9b1119bff87a1bfcacea
username000101 <cxx.username000101@gmail.com>
2024-12-14 21:39:19 +0300

    Call meetx::MainLoop to start the bot

--- main.cpp
+++ main.cpp
@@ -3,10 +3,13 @@
 #include <Args.hpp>
 #include <Definitions.hpp>
 #include <Init.hpp>
+#include <Logic.hpp>
 
 int main(int argc, char* argv[]) {
     init::initLoggers();
     ah::Args args(argc, argv);
     args.process(ah::Args::ArgsStyle::LIKE_CMAKE);
     init::MeetXInit(args);
+    meetx::MeetXStart();
+    meetx::MainLoop();
 }
