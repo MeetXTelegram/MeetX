7f1409b82d2387d109f7981771f1eaac016b31bc
username000101 <cxx.username000101@gmail.com>
2024-12-14 17:19:41 +0300

    Database creation added

--- include/Init.hpp
+++ include/Init.hpp
@@ -21,7 +21,7 @@ namespace init {
         std::ostringstream formattedTime; formattedTime << std::put_time(std::localtime(&nowTime_C), "%F_%H_%M");
 
         auto stdoutSink = createStdoutSink;
-        auto fileSink = createFileSink(meetxLogsFolder + "/MEETX_LOG_" + formattedTime.str());
+        auto fileSink = createFileSink(meetxLogsFolder + "/MEETX_LOG_" + formattedTime.str() + ".txt");
 
         loggingUtils::createLogger("SignalHandler", stdoutSink, fileSink, spdlog::level::trace);
         loggingUtils::createLogger("MAIN_LOGGER", stdoutSink, fileSink, spdlog::level::trace);
@@ -41,19 +41,48 @@ namespace init {
 
         std::signal(SIGUSR1, signalsUtils::SIGUSR1Handler);
         std::set_terminate(exceptionsUtils::TerminateHandler);
-
         if (!argsHandler.getArg("dbname").has_value()) {
             spdlog::log(spdlog::level::critical, "Failed to init MeetX: the \"dbname\" argument is not provided");
             std::raise(SIGUSR1);
         }
         auto dbname = argsHandler.getArg("dbname").value().second;
 
-        /*
         if (!argsHandler.getArg("password").has_value()) {
             spdlog::log(spdlog::level::critical, "Failed to init MeetX: the \"password\" argument is not provided");
             std::raise(SIGUSR1);
         }
         auto password = argsHandler.getArg("password").value().second;
-        */
+
+        if (!argsHandler.getArg("user").has_value()) {
+            spdlog::log(spdlog::level::critical, "Failed to init MeetX: the \"user\" argument is not provided");
+            std::raise(SIGUSR1);
+        }
+        auto user = argsHandler.getArg("user").value().second;
+
+        try {
+            db::database = std::make_shared<pqxx::connection>(std::string("dbname=" + dbname + " user=" + user + " password=" + password));
+            pqxx::work w(*db::database);
+            w.exec("CREATE TABLE IF NOT EXISTS users("
+                   "id BIGINT PRIMARY KEY,"
+                   "tgFirstName VARCHAR NOT NULL,"
+                   "tgLastName VARCHAR,"
+                   "age SMALLINT,"
+                   "name VARCHAR NOT NULL,"
+                   "bio VARCHAR,"
+                   "gender SMALLINT NOT NULL,"
+                   "language SMALLINT NOT NULL DEFAULT 2,"
+                   "city VARCHAR,"
+                   "media JSONB,"
+                   "preferredLanguages JSONB,"
+                   "preferredAges JSONB,"
+                   "preferredCities JSONB,"
+                   "preferredGenders JSONB,"
+                   "hasProfile BOOLEAN NOT NULL DEFAULT FALSE"
+                   ");");
+            w.commit();
+        } catch (std::exception& dberr) {
+            spdlog::log(spdlog::level::critical, "Failed to init MeetX: pqxx created an exception: {}", dberr.what());
+            std::raise(SIGUSR1);
+        }
     }
 }
